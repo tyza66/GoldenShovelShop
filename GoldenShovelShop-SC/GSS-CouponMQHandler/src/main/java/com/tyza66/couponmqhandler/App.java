1bf51f9f61092514b222dfb300a8c9e638d9fea8
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-07-12 10:42:49 +0800

    更新

---
+++ GoldenShovelShop-SC/GSS-CouponMQHandler/src/main/java/com/tyza66/couponmqhandler/App.java
@@ -0,0 +1,13 @@
+package com.tyza66.couponmqhandler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
